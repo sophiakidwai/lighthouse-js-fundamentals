a44f328b6d744d3db3faed8f853479e2562ca859
sophia kidawai <sophia.kidwai@telus.com>
2022-11-05 23:19:49 -0700

    Is Odd activity

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function (num){
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(2));
+console.log(isOdd(5));
+console.log(isOdd(8));
+console.log(isOdd(13));
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
